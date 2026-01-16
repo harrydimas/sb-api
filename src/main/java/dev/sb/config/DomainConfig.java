22cc32fbaef1f007dba71a9d2a9bcdb46bc839cf
dimas <dimasharry07@gmail.com>
2026-01-16 16:58:44 +0700

    downgrade spring boot to v3.x.x

--- src/main/java/dev/sb/config/DomainConfig.java
+++ src/main/java/dev/sb/config/DomainConfig.java
@@ -3,7 +3,7 @@ package dev.sb.config;
 
 import java.time.OffsetDateTime;
 import java.util.Optional;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
